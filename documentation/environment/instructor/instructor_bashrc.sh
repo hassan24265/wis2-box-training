1bc8cb1c123ffa3236e27f09ecef0550ec45a340
hassan24265 <142603733+hassan24265@users.noreply.github.com>
2024-08-20 17:48:28 +0200

    Delete documentation/environment directory

--- documentation/environment/instructor/instructor_bashrc.sh
+++
@@ -1,5 +0,0 @@
-# execute the below to remove color formatting from instructor VM
-# in order to improve display when projecting
-
-export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[01;32m\]:\$ "
-unalias ls
